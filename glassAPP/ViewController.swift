348d56455996690742cfce20b3d4197fbd715dd3
Aula11 <aula11@imac-13.local>
2019-12-03 17:15:30 +0100

    Initial Commit

---
+++ glassAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  glassAPP
+//
+//  Created by Aula11 on 3/12/19.
+//  Copyright © 2019 Jose Manuel Martinez Salas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
